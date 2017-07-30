72bb7f1087eb80e0ce04ce34ddec8aa18cc73649
Naveen Aggarwal <v-naagga@microsoft.com>
2017-07-30 13:47:50 -0700

    Singleton Design pattern

---
+++ Singleton/Singleton/EarlySingleton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class EarlySingleton
+    {
+        private static EarlySingleton singletonInstance = new EarlySingleton();
+
+        private EarlySingleton()
+        {
+        }
+
+        public EarlySingleton getInstance()
+        {
+            return singletonInstance;
+        }        
+    }
+}
---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+        }
+    }
+    public class Singleton
+    {
+        private static Singleton _instance;
+
+        private Singleton() { 
+        }
+
+        public static Singleton getInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            else
+                return _instance;
+        }
+    }
+}
