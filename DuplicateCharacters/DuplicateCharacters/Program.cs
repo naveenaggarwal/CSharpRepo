526edaa9dd3bdc67cbe733973f84a2502ab6bc93
Naveen Aggarwal <v-naagga@microsoft.com>
2017-08-02 15:39:57 -0700

    partial View MVC.NET

---
+++ DuplicateCharacters/DuplicateCharacters/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplicateCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Call the function by passing a string for removing the duplicates");
+            Console.WriteLine(RemoveduplicateChars("Google"));
+            Console.ReadLine();
+        }
+
+        static string RemoveduplicateChars(string input)
+        {
+            string duplicates = "";
+
+            string uniquechars = "";
+
+            foreach(char c in input)
+            {
+                if (duplicates.IndexOf(c) == -1)
+                {
+                    duplicates = duplicates + c;
+                    uniquechars = uniquechars + c;
+                }
+            }
+            return duplicates;
+        }
+    }
+}
